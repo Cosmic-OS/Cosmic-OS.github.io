01993247deca03874c70c8cce873ec458c91a96a
Sid-Sun <sidharth.soni525@gmail.com>
2018-03-13 18:59:07 +0530

    Beautify
    Beautiful; isn't she?
    
    Signed-off-by: Sid-Sun <sidharth.soni525@gmail.com>

--- js/accordion.js
+++ js/accordion.js
@@ -2,14 +2,13 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    });
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
 }
-
