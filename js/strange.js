f37e15b5252cdf48fb7ed57aeb1380ce116eebab
Sid-Sun <sid-sun@outlook.com>
2018-09-10 15:45:37 +0530

    Make transition to localStorage seemless.
    If there is already a light theme cookie, set localstorage before deleting it else, delete it.
    
    Signed-off-by: Sid-Sun <sid-sun@outlook.com>

--- js/strange.js
+++ js/strange.js
@@ -20,6 +20,12 @@ function linkFetch(device) {
         }
     });
 }
+if (getCookie('themeStatus') === "light") {
+    localStorage.setItem("theme", "light");
+    deleteCookie('themeStatus');
+} else {
+    deleteCookie('themeStatus');
+}
 var isDevicePage;
 function checkThemeStatus() {
     var themeStatus = localStorage.getItem("theme");
@@ -70,11 +76,20 @@ function applyTheme(theme){
     document.getElementById("styleSheet").href = themeName + ".css";
 }
 /* Let's delete cookies for people who have it set */
-deleteCookie('themeStatus');
 function deleteCookie(cname) {
     var d = new Date(); //Create an date object
     d.setTime(d.getTime() - (1000*60*60*24)); //Set the time to the past. 1000 milliseonds = 1 second
     var expires = "expires=" + d.toGMTString(); //Compose the expirartion date
     window.document.cookie = cname+"="+"; "+expires;//Set the cookie with name and the expiration date
 
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
 }
