98f20795b78d0595e932861bee8dcc8e08a44608
Sid-Sun <sid-sun@outlook.com>
2018-09-10 15:27:33 +0530

    Delete cookie for those who have it set.
    We don't want unwanted cookies flyin' around, do we? Nope.
    
    Signed-off-by: Sid-Sun <sid-sun@outlook.com>

--- js/strange.js
+++ js/strange.js
@@ -69,30 +69,12 @@ function applyTheme(theme){
     }
     document.getElementById("styleSheet").href = themeName + ".css";
 }
-/*
-function setCookie(name,value,days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
-}
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
-}
+/* Let's delete cookies for people who have it set */
+deleteCookie('themeStatus');
 function deleteCookie(cname) {
     var d = new Date(); //Create an date object
     d.setTime(d.getTime() - (1000*60*60*24)); //Set the time to the past. 1000 milliseonds = 1 second
     var expires = "expires=" + d.toGMTString(); //Compose the expirartion date
     window.document.cookie = cname+"="+"; "+expires;//Set the cookie with name and the expiration date
 
-}*/
+}
